0b633368f5dad18483d34d6cfd5d6b4bf0991fdf
Josh Nichols <josh.nichols@gusto.com>
2023-02-13 12:05:43 -0500

    move notification logic to Notifier class

--- lib/prosopite.rb
+++ lib/prosopite.rb
@@ -124,6 +124,59 @@ module Prosopite
         PgQuery.fingerprint(query)
       end
     end
+  end
+
+  class Notifier
+    extend Forwardable
+
+    attr_accessor :configuration
+
+    def initialize(configuration:)
+      @configuration = configuration
+    end
+
+    def_delegators :configuration,
+      :rails_logger, :custom_logger, :backtrace_cleaner, :stderr_logger, :prosopite_logger, :raise?
+
+
+    def send_notifications
+      notifications_str = ''
+
+      tc[:prosopite_notifications].each do |queries, kaller|
+        notifications_str << "N+1 queries detected:\n"
+
+        queries.each { |q| notifications_str << "  #{q}\n" }
+
+        notifications_str << "Call stack:\n"
+        kaller = backtrace_cleaner.clean(kaller)
+        kaller.each do |f|
+          notifications_str << "  #{f}\n"
+        end
+
+        notifications_str << "\n"
+      end
+
+      custom_logger.warn(notifications_str) if custom_logger
+
+      Rails.logger.warn(red(notifications_str)) if rails_logger
+      $stderr.puts(red(notifications_str)) if stderr_logger
+
+      if prosopite_logger
+        File.open(File.join(Rails.root, 'log', 'prosopite.log'), 'a') do |f|
+          f.puts(notifications_str)
+        end
+      end
+
+      raise NPlusOneQueriesError.new(notifications_str) if raise?
+    end
+
+    def tc
+      Thread.current
+    end
+
+    def red(str)
+      str.split("\n").map { |line| "\e[91m#{line}\e[0m" }.join("\n")
+    end
 
   end
 
@@ -258,38 +311,7 @@ module Prosopite
     end
 
     def send_notifications
-      notifications_str = ''
-
-      tc[:prosopite_notifications].each do |queries, kaller|
-        notifications_str << "N+1 queries detected:\n"
-
-        queries.each { |q| notifications_str << "  #{q}\n" }
-
-        notifications_str << "Call stack:\n"
-        kaller = backtrace_cleaner.clean(kaller)
-        kaller.each do |f|
-          notifications_str << "  #{f}\n"
-        end
-
-        notifications_str << "\n"
-      end
-
-      custom_logger.warn(notifications_str) if custom_logger
-
-      Rails.logger.warn(red(notifications_str)) if rails_logger
-      $stderr.puts(red(notifications_str)) if stderr_logger
-
-      if prosopite_logger
-        File.open(File.join(Rails.root, 'log', 'prosopite.log'), 'a') do |f|
-          f.puts(notifications_str)
-        end
-      end
-
-      raise NPlusOneQueriesError.new(notifications_str) if raise?
-    end
-
-    def red(str)
-      str.split("\n").map { |line| "\e[91m#{line}\e[0m" }.join("\n")
+      Notifier.new(configuration: configuration).send_notifications
     end
 
     def ignore_query?(sql)
